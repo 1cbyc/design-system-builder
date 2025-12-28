9d531370d25cb030f9420659ee79fca603fab7c3
1cbyc <isaacnsisong@gmail.com>
2025-12-27 16:14:41 -0800

    fix(server): Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,13 @@ import { router } from './trpc'
 import { projectRouter } from './routers/project'
 import { componentRouter } from './routers/component'
 import { themeRouter } from './routers/theme'
+import { pageRouter } from './routers/page'
 
 export const appRouter = router({
   project: projectRouter,
   component: componentRouter,
   theme: themeRouter,
+  page: pageRouter,
 })
 
 export type AppRouter = typeof appRouter
